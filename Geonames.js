06f76fa7ee150ded195a5aee51f8491e159d9ee0
Ncos95 <67147234+Ncos95@users.noreply.github.com>
2020-09-14 16:01:10 +0100

    Add files via upload

---
+++ Geonames.js
@@ -0,0 +1,29 @@
+$('#button').click(function() {
+
+    $.ajax({
+        url: "libraries/Geonames.php",
+        type: 'POST',
+        dataType: 'json',
+        data: {
+            country: $('#country').val(),
+            lang: $('#language').val()
+        },
+        success: function(result) {
+
+            console.log(result);
+
+            if (result.status.name == "ok") {
+
+                $('#Capital').html(result['data'][0]['capital']);
+                $('#Population').html(result['data'][0]['population']);
+
+            }
+        
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            Console.log('An error has occured, please try again.');
+        }
+    }); 
+
+
+});
